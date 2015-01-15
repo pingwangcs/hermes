7f52b3ce4b041d686d17d827c633bae37291240a
RainbowDragon <xuanxiang@hotmail.com>
2015-01-14 20:15:44 -0800

    initial project creation

---
+++ Hermes/Hermes/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Hermes
+//
+//  Created by XuanXie on 1/14/15.
+//  Copyright (c) 2015 XuanXie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ Hermes/Hermes/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Hermes
+//
+//  Created by XuanXie on 1/14/15.
+//  Copyright (c) 2015 XuanXie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
